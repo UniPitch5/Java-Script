936cd704de45d1abc4c0b7361972af951808bf8d
UniPitch5 <abcdsarvesh97@gmail.com>
2026-05-04 05:17:50 +0000

    Add initial JavaScript files with console logging and variable declarations

---
+++ variables.js
@@ -0,0 +1,12 @@
+const name = "Sarvesh"
+let phone = 8439101153
+var email = "Sarve@gmail.com"
+city = "Delhi"
+
+// name = "Sarvesh"
+phone = 8439101163
+email = "Sarvesh@gmail.com"
+city = "Dehradun"
+
+console.log(name)
+console.table([name, phone, email, city])
